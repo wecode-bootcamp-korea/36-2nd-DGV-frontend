a91f9fc93e0b53765d030c46549753d3f06c8497
Qrio <kyuchoi@YUNMIui-MacBookAir.local>
2022-09-07 23:29:48 +0900

    무비차트 완료

--- src/pages/Moviechart/SubMovieChart/MovieList.js
+++ src/pages/Moviechart/SubMovieChart/MovieList.js
@@ -17,32 +17,37 @@ function MovieList() {
       method: 'GET',
     })
       .then(response => response.json())
-      .then(result => setMovieInfo(result.listByRating));
+      .then(result => setMovieInfo(result.orderList));
   }, []);
   return (
     <div className="movieList">
       <MovieCard>
-        {movieInfo.map(({ id, img, title, eng_title, description }) => {
-          return (
-            <MovieInfoWrap key={id} onClick={() => toMovieDetail(id)}>
-              <Img src={img} onClick={() => toMovieDetail(id)} />
-              <div className="movieDetailWrap">
-                <MovieTitle onClick={() => toMovieDetail(id)}>
-                  {title}
-                </MovieTitle>
-                <div className="movieInfo">
-                  <MovieEngTitle onClick={() => toMovieDetail(id)}>
-                    {eng_title}
-                  </MovieEngTitle>
-                  <MovieGenre onClick={() => toMovieDetail(id)}>
-                    {description}
-                  </MovieGenre>
+        {movieInfo.map(
+          ({ id, thumbnail_image_url, title, eng_title, description }) => {
+            return (
+              <MovieInfoWrap key={id} onClick={() => toMovieDetail(id)}>
+                <Img
+                  src={thumbnail_image_url}
+                  onClick={() => toMovieDetail(id)}
+                />
+                <div className="movieDetailWrap">
+                  <MovieTitle onClick={() => toMovieDetail(id)}>
+                    {title}
+                  </MovieTitle>
+                  <div className="movieInfo">
+                    <MovieEngTitle onClick={() => toMovieDetail(id)}>
+                      {eng_title}
+                    </MovieEngTitle>
+                    <MovieGenre onClick={() => toMovieDetail(id)}>
+                      {description}
+                    </MovieGenre>
+                  </div>
+                  <Button />
                 </div>
-                <Button />
-              </div>
-            </MovieInfoWrap>
-          );
-        })}
+              </MovieInfoWrap>
+            );
+          }
+        )}
       </MovieCard>
     </div>
   );
@@ -68,22 +73,29 @@ const MovieInfoWrap = styled.div`
 
 const Img = styled.img`
   width: 197px;
+  height: 295.5px;
+  margin-bottom: 5px;
 `;
 
 const MovieTitle = styled.span`
   display: block;
+  margin-bottom: 5px;
   color: #333333;
+  font-size: 20px;
+  font-weight: 600;
   white-space: nowrap;
 `;
 
 const MovieEngTitle = styled.span`
+  margin-right: 10px;
   color: #333333;
-  font-size: 11px;
+  font-size: 16px;
+  font-weight: 600;
   white-space: nowrap;
 `;
 
 const MovieGenre = styled.span`
   color: #333333;
-  font-size: 11px;
+  font-size: 16px;
   white-space: nowrap;
 `;
